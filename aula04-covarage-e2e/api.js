285d94a01a4bb1c9e73545602961fbd5ce74d717
jhonataT <jhonata.tenorio2526@gmail.com>
2023-01-20 00:32:03 -0300

    Finishing e2e class

--- aula04-covarage-e2e/api.js
+++ aula04-covarage-e2e/api.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const DEFAULT_USER = { username: 'jhonataT', password: '123' };
 
 const routes = {
     '/contact:get': (req, res) => {
         res.write('Contact us page');
+        return res.end();
+    },
+    '/login:post': async (req, res) => {
+        // res é um iterator!
+        for await (const data of req) {
+            const user = JSON.parse(data);
+            if(
+                user.username !== DEFAULT_USER.username ||
+                user.password !== DEFAULT_USER.password    
+            ) {
+                res.writeHead(401);
+                res.write('logging failed!');
+                return res.end();
+            }
+        }
+        res.write('logging has succeeded');
         return res.end();
     },
     default: (req, res) => {
@@ -14,7 +31,6 @@ const routes = {
 const handler = (req, res) => {
     const { url, method } = req;
     const routeKey = `${url}:${method.toLowerCase()}`;
-    console.log(routeKey);
     const chosen = routes[routeKey] || routes.default;
     res.writeHead(200, {
         'Content-Type': 'text/html'
--- aula04-covarage-e2e/api.test.js
+++ aula04-covarage-e2e/api.test.js
@@ -1,5 +1,6 @@
 const { describe, it } = require('mocha');
 const request = require('supertest');
+const assert = require('assert');
 const app = require('./api');
 
 describe('API Suit test', () => {
@@ -7,10 +8,49 @@ describe('API Suit test', () => {
         it(
             'should request the contact page and return HTTP Status 200',
             async () => {
-                const reponse = await request(app)
+                const response = await request(app)
                     .get('/contact')
                     .expect(200);
-                console.log('response', response);
+
+                assert.deepStrictEqual(response.text, 'Contact us page');
+            }
+        )
+    })
+
+    describe('/hello', () => {
+        it(
+            'should request an inexistent route /hi and redirect to /hello',
+            async () => {
+                const response = await request(app)
+                    .get('/hi')
+                    .expect(200);
+                assert.deepStrictEqual(response.text, 'Hello World!')
+            }
+        )
+    })
+
+    describe('/login', () => {
+        it(
+            'should login successfully on the login and return HTTP Status 200',
+            async () => {
+                const response = await request(app)
+                    .post('/login')
+                    .send({ username: "jhonataT", password: "123" })
+                    .expect(200)
+
+                assert.deepStrictEqual(response.text, 'logging has succeeded')
+            }
+        );
+        it(
+            'should unauthorize a request when requesting it using wrong credentials and return HTTP Status 401',
+            async () => {
+                const response = await request(app)
+                    .post('/login')
+                    .send({ username: 'ghostUser', password: '432'})
+                    .expect(401)
+
+                assert.ok(response.unauthorized)
+                assert.deepStrictEqual(response.text, 'logging failed!')
             }
         )
     })
